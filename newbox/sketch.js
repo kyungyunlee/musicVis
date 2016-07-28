193dc9da87ad38832b2e9befadcc6aa45c284718
kyungyunlee <kyungyunlee2393@gmail.com>
2016-07-28 23:28:55 +0900

    added splice to fix slowdown and implemented rotating camera

--- newbox/sketch.js
+++ newbox/sketch.js
@@ -23,9 +23,9 @@ var beatThreshold = 0.4;
 var beatCutOff=0;
 var beatDecayRate=0.98;
 var countFramesSinceLastBeat=0;
-
+var angle;
 function preload() {
-  song = loadSound('assets/lifeincolor.mp3');
+  song = loadSound('assets/colours.mp3');
 }
 
 function setup() {
@@ -36,40 +36,40 @@ function setup() {
   amplitude.setInput(song);
   amplitude.smooth(0.9);
 
-  sizeSlider = createSlider(30,300,300,0.1);
+  sizeSlider = createSlider(50,300,300,0.1);
   sizeSlider.position(300,windowHeight-30);
   
   boxColor = color(random(20,40), random(100,120), random(140,180));
-
+  angle=0;
 }
 
 function draw() {
-  var timer = int(millis() / 1000);
+  var timer = int(millis()/1000);
+  // var timer = frameCount;
   print(timer);
   background(20);
   ambientLight(150);
   pointLight(250, 250, 250, 200, 300, 100);
-
-  camera(moveCameraX, moveCameraY, cameraZ);
+  
   if(timer>0){
-    if (timer%40==0){
-      moveCameraX-=60;
-      moveCameraY-=20;
-    }
-    else if (timer%30==0){
-      moveCameraX-=75;
-      moveCameraY+=45;
+    if (timer%30==0){
+      angle-=1;
+      moveCameraY+=15;
     }
     else if (timer%20==0){
-      moveCameraX+=50;
-      moveCameraY-=20;
+      angle +=2;
+      moveCameraY -=20;
     }
     else if (timer%10==0){
-      moveCameraX+=25;
-      moveCameraY-=20;
+      angle -=1;
+      moveCameraY +=5;
     }
   }
-
+  
+  rotateY(radians(angle));
+  camera(moveCameraX, moveCameraY, cameraZ);
+  
+  
   var boxheight = map(amplitude.getLevel(), 0, 1, 30, 2000);
   newbox.push(new NewBox(0, 0, zpos, sizeSlider.value(), boxheight, sizeSlider.value(),boxColor));
   newbox2.push(new NewBox(1000, 0, zpos, sizeSlider.value(), boxheight, sizeSlider.value(), boxColor));
@@ -87,6 +87,10 @@ function draw() {
     zpos -= sizeSlider.value() + 10;
     cameraZ -= sizeSlider.value();
   }
+  if (newbox.length>100){
+      newbox.splice(0,1);
+      newbox2.splice(0,1);
+    }
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
